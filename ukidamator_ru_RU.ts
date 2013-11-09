f369c42edb3a0898b6c383e8f22ea7e61cbfedb2
syjgin <gruz103@gmail.com>
2013-11-09 22:31:59 +0400

    translation added

--- ukidamator_ru_RU.ts
+++ ukidamator_ru_RU.ts
@@ -9,54 +9,116 @@
         <translation>Юкидаматор</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="44"/>
+        <location filename="programwindow.ui" line="48"/>
+        <location filename="programwindow.ui" line="314"/>
         <source>&amp;Copy</source>
         <translation>&amp;Копировать</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="57"/>
+        <location filename="programwindow.ui" line="61"/>
+        <location filename="programwindow.ui" line="319"/>
         <source>&amp;Paste</source>
         <translation>&amp;Вставить</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="73"/>
+        <location filename="programwindow.ui" line="77"/>
         <source>2&amp;yukidama</source>
         <translation>Сне&amp;жифицировать</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="83"/>
+        <location filename="programwindow.ui" line="87"/>
         <source>Kana</source>
         <translation>Кана</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="95"/>
+        <location filename="programwindow.ui" line="99"/>
         <source>Hiragana</source>
         <translation>Хирагана</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="111"/>
+        <location filename="programwindow.ui" line="115"/>
         <source>Katakana</source>
         <translation>Катакана</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="121"/>
+        <location filename="programwindow.ui" line="128"/>
         <source>Translation</source>
         <translation>Перевод</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="127"/>
+        <location filename="programwindow.ui" line="134"/>
         <source>Kiridzi</source>
         <translation>Киридзи</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="137"/>
+        <location filename="programwindow.ui" line="144"/>
         <source>Romadzi</source>
         <translation>Ромадзи</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="159"/>
+        <location filename="programwindow.ui" line="169"/>
         <source>Possibility</source>
         <translation>Вероятность</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="267"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="275"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Редактировать</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="282"/>
+        <source>&amp;Translation</source>
+        <translation>&amp;Перевод</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="291"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Помощь</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="309"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="327"/>
+        <source>&amp;Hiragana</source>
+        <translation>&amp;Хирагана</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="335"/>
+        <source>&amp;Katakana</source>
+        <translation>&amp;Катакана</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="343"/>
+        <source>&amp;Romadzi</source>
+        <translation>&amp;Ромадзи</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="351"/>
+        <source>&amp;Kiridzi</source>
+        <translation>&amp;Киридзи</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="361"/>
+        <source>About &amp;ukidamator</source>
+        <translation>О &amp;юкидаматоре</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="366"/>
+        <source>About &amp;Qt</source>
+        <translation>О &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="programwindow.ui" line="371"/>
+        <source>Donate</source>
+        <translation>Пожертвовать</translation>
     </message>
     <message>
         <source>10%</source>
@@ -67,19 +129,24 @@
         <translation type="obsolete">100%</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="220"/>
+        <location filename="programwindow.ui" line="304"/>
         <source>&amp;Save</source>
         <translation>&amp;Сохранить</translation>
     </message>
     <message>
-        <location filename="programwindow.ui" line="233"/>
+        <location filename="programwindow.ui" line="230"/>
+        <location filename="programwindow.ui" line="356"/>
         <source>&amp;Close</source>
         <translation>&amp;Закрыть</translation>
     </message>
     <message>
-        <location filename="programwindow.cpp" line="46"/>
         <source>Text Files (*.txt);;All Files (*)</source>
-        <translation>Текстовые файлы (*.txt);;Все файлы(*)</translation>
+        <translation type="obsolete">Текстовые файлы (*.txt);;Все файлы(*)</translation>
+    </message>
+    <message>
+        <location filename="programwindow.cpp" line="179"/>
+        <source>Ukidamator is small education program. Put text into main text field and press &quot;2yukidama&quot; button, and as result you can see, that some of symbols are converted to hiragana or katakana. Now you can learn yourself by reading this mixed text. Enjoy:)</source>
+        <translation>Юкидаматор - небольшая программа для обучения японскому. Вставьте текст в поле ввода, настройте параметры и нажмите кнопку &quot;Снежифицировать&quot;. В результате получится обучающий текст, который можно читать вслух для тренировки скорости распознавания катаканы или хираганы. Надеюсь, вам понравится:)</translation>
     </message>
 </context>
 </TS>
